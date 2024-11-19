488aacf653910a73d673b003168665f60ffdae16
dn5772 <shendanny@naver.com>
2024-11-19 17:06:40 +0900

    :sparkles: [ Feat ] Negotiation repository 추가

---
+++ src/main/java/com/checkhouse/core/repository/mysql/NegotiationRepository.java
@@ -0,0 +1,30 @@
+package com.checkhouse.core.repository.mysql;
+
+import com.checkhouse.core.entity.Negotiation;
+import com.checkhouse.core.entity.enums.NegotiationState;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface NegotiationRepository extends JpaRepository<Negotiation, UUID> {
+    // ID 조회
+    Optional<Negotiation> findById(UUID negotiationId);
+
+    // 구매자 ID 조회
+    @Query("select up from Negotiation up where up.buyer = :buyerId and up.deletedDate is null")
+    List<Negotiation> findAllByBuyerId(UUID buyerId);
+
+    // 판매자 ID 조회
+    @Query("select up from Negotiation up where up.seller = :sellerId and up.deletedDate is null")
+    List<Negotiation> findAllBySellerId(UUID sellerId);
+
+    // 상태 조회    
+    @Query("select up from Negotiation up where up.state = :state and up.deletedDate is null")
+    List<Negotiation> findAllByState(NegotiationState state);
+
+
+
+}
